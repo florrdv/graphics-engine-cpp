2a240e8af8f83fd9dbc08cfbf9de85aa4106ab87
Florrdv <flor@ronsmans.me>
2022-03-12 19:04:51 -0600

    Finished Figure class

--- util/Figure.h
+++ util/Figure.h
@@ -1,4 +1,11 @@
 #ifndef __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FIGURE_H_
 #define __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FIGURE_H_
 
+#include <vector>
+
+class Figure {
+public:
+    std::vector<int> pointIndexes;
+};
+
 #endif // __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FIGURE_H_
