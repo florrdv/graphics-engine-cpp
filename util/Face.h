a2b752f54d08fc406e7f772d8fde49b78563df85
Florrdv <flor@ronsmans.me>
2022-03-12 19:06:55 -0600

    Finished implementing new classes

--- util/Face.h
+++ util/Face.h
@@ -1,4 +1,11 @@
 #ifndef __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FACE_H_
 #define __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FACE_H_
 
+#include <vector>
+
+class Face { 
+public: 
+    std::vector<int> pointIndexes;
+};
+
 #endif // __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FACE_H_
--- util/Figure.h
+++ util/Figure.h
@@ -3,9 +3,16 @@
 
 #include <vector>
 
+#include "Face.h"
+#include "Color.h"
+
+#include "../lib/vector3d/vector3d.h"
+
 class Figure {
 public:
-    std::vector<int> pointIndexes;
+    std::vector<Vector3D> points;
+    std::vector<Face> faces;
+    Color color;
 };
 
 #endif // __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_FIGURE_H_
