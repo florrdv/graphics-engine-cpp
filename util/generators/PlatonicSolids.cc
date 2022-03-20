d0b0a13492ed3b3e766d494f198962988b9f0042
Florrdv <flor@ronsmans.me>
2022-03-20 17:57:58 +0100

    Finished createTetrahedron

--- util/generators/PlatonicSolids.cc
+++ util/generators/PlatonicSolids.cc
@@ -1,14 +1,16 @@
 #include "../Figure.h"
 #include "../Face.h"
 
-Figure createTetrahedron() {
+Figure createTetrahedron(Color c) {
     Vector3D p0 = Vector3D::point(1, -1, -1);
     Vector3D p1 = Vector3D::point(-1, 1, -1);
     Vector3D p2 = Vector3D::point(1, 1, 1);
     Vector3D p3 = Vector3D::point(-1, -1, 1);
 
-    Face f1 = Face({0, 1, 2});
-    Face f2 = Face({1, 3, 2});
-    Face f3 = Face({0, 3, 1});
-    Face f4 = Face({0, 2, 3});
+    Face f0 = Face({0, 1, 2});
+    Face f1 = Face({1, 3, 2});
+    Face f2 = Face({0, 3, 1});
+    Face f3 = Face({0, 2, 3});
+
+    return Figure({p0, p1, p2, p3}, {f0, f1, f2, f3}, c);
 }
