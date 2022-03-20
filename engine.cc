83b3c5a8f5191b1387a447679267a6826e9c0f15
Florrdv <flor@ronsmans.me>
2022-03-20 19:28:30 +0100

    Started working on 3D L systems

--- engine.cc
+++ engine.cc
@@ -286,7 +286,7 @@ struct Triplet {
 };
 
 
-void drawLSystem(const LParser::LSystem2D& l_system, Lines2D& lines, const Color color, std::string current = "", int it = 0) {
+void draw2DLSystem(const LParser::LSystem2D& l_system, Lines2D& lines, const Color color, std::string current = "", int it = 0) {
     int iterations = l_system.get_nr_iterations();
     std::string initiator = l_system.get_initiator();
     std::set<char> alphabet = l_system.get_alphabet();
@@ -341,7 +341,7 @@ void drawLSystem(const LParser::LSystem2D& l_system, Lines2D& lines, const Color
         }
     }
 
-    drawLSystem(l_system, lines, color, replaced, it + 1);
+    draw2DLSystem(l_system, lines, color, replaced, it + 1);
 }
 
 img::EasyImage LSystem(const ini::Configuration& configuration) {
@@ -366,11 +366,69 @@ img::EasyImage LSystem(const ini::Configuration& configuration) {
     input_stream.close();
 
     Lines2D lines;
-    drawLSystem(l_system, lines, color);
+    draw2DLSystem(l_system, lines, color);
     img::EasyImage img = draw2DLines(lines, size, backgroundColor);
 
 
     return img;
+}
+
+void draw3DLSystem(const LParser::LSystem2D& l_system, Lines2D& lines, const Color color, std::string current = "", int it = 0) {
+    int iterations = l_system.get_nr_iterations();
+    std::string initiator = l_system.get_initiator();
+    std::set<char> alphabet = l_system.get_alphabet();
+
+    if (current == "") current = initiator;
+
+    if (it == iterations) {
+        double angle = l_system.get_starting_angle();
+        double angleOffset = l_system.get_angle();
+
+        std::stack<Triplet> stack;
+        double x = 0;
+        double y = 0;
+
+
+        for (char c : current) {
+            if (c == '+') angle += angleOffset;
+            else if (c == '-') angle -= angleOffset;
+            else if (c == '(') stack.push(Triplet{ x, y, angle });
+            else if (c == ')') {
+                Triplet saved = stack.top();
+                stack.pop();
+
+                x = saved.first;
+                y = saved.second;
+                angle = saved.third;
+            }
+            else if (alphabet.find(c) != alphabet.end()) {
+                if (l_system.draw(c)) {
+                    Point2D p1 = Point2D(x, y);
+
+                    x += std::cos(angle * M_PI / 180);
+                    y += std::sin(angle * M_PI / 180);
+
+                    Point2D p2 = Point2D(x, y);
+
+                    Line2D line = Line2D(p1, p2, color);
+                    lines.push_back(line);
+                }
+            }
+            else std::cout << ("⛔️| Invalid character " + std::to_string(c) + " in l-system description") << std::endl;
+        }
+
+        return;
+    }
+
+    std::string replaced = "";
+    for (char c : current) {
+        if (alphabet.find(c) == alphabet.end()) replaced += c;
+        else {
+            replaced += l_system.get_replacement(c);
+        }
+    }
+
+    draw2DLSystem(l_system, lines, color, replaced, it + 1);
 }
 
 img::EasyImage wireFrame(const ini::Configuration& c) {
@@ -443,9 +501,10 @@ img::EasyImage wireFrame(const ini::Configuration& c) {
                 figure = Figure(vectors, faces, color);
 
             }
-        }
-        else if (type == "Cube") {
+        } else if (type == "Cube") {
             figure = PlatonicSolids::createCube(color);
+        } else if (type == "3DLSystem") { 
+
         }
 
 
