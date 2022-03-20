050308399755db85a7a7833657df99d2cba290db
Florrdv <flor@ronsmans.me>
2022-03-20 22:54:50 +0100

    Added sphere generation

--- util/generators/PlatonicSolids.cc
+++ util/generators/PlatonicSolids.cc
@@ -1,6 +1,8 @@
 #ifndef __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_GENERATORS_PLATONICSOLIDS_CC_
 #define __PROJECTS_GRAPHICS_ENGINE_CPP_UTIL_GENERATORS_PLATONICSOLIDS_CC_
 
+#include <cmath>
+
 #include "../Figure.h"
 #include "../Face.h"
 
@@ -38,6 +40,52 @@ namespace PlatonicSolids {
         Face f5 = Face({ 0, 5, 1, 4 });
 
         return Figure({ p0, p1, p2, p3, p4, p5, p6, p7 }, { f0, f1, f2, f3 }, c);
+    }
+
+    Figure createSphere(Color c, const double radius, const int n) {
+        Figure intermediate = createTetrahedron(c);
+
+        for (int i = 0; i < n; i++) {
+            std::vector<Face> newFaces;
+            for (auto face : intermediate.faces) {
+                int aIndex = face.pointIndexes[0];
+                int bIndex = face.pointIndexes[1];
+                int cIndex = face.pointIndexes[2];
+                Vector3D a = intermediate.points[face.pointIndexes[0]];
+                Vector3D b = intermediate.points[face.pointIndexes[1]];
+                Vector3D c = intermediate.points[face.pointIndexes[2]];
+
+                Vector3D d = (a + b) / 2.0;
+                Vector3D e = (a + c) / 2.0;
+                Vector3D f = (b + c) / 2.0;
+
+                int dIndex = intermediate.points.size();
+                int eIndex = intermediate.points.size();
+                int fIndex = intermediate.points.size();
+                intermediate.points.push_back(d);
+                intermediate.points.push_back(e);
+                intermediate.points.push_back(f);
+
+                newFaces.push_back(Face({ aIndex, dIndex, eIndex }));
+                newFaces.push_back(Face({ bIndex, fIndex, dIndex }));
+                newFaces.push_back(Face({ cIndex, eIndex, fIndex }));
+                newFaces.push_back(Face({ dIndex, fIndex, eIndex }));
+            }
+
+            intermediate.faces = newFaces;
+        }
+
+        for (Vector3D &point : intermediate.points) { 
+            double x = point.x;
+            double y = point.y;
+            double z = point.z;
+            double r = std::sqrt(x * x +  y*y + z*z);
+            point.x = x / r;
+            point.y = y / r;
+            point.z = z / r;
+        }
+
+        return intermediate;
     }
 }
 
