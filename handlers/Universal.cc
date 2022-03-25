c674b7a17f8520310a00b830475cdee7107ae92c
Florrdv <flor@ronsmans.me>
2022-03-25 16:07:57 +0100

    Started major rewrite

---
+++ handlers/Universal.cc
@@ -0,0 +1,132 @@
+void applyTransformation(Figure& fig, const Matrix& m) {
+    for (auto& p : fig.points) p *= m;
+}
+
+void applyTransformationAll(Figures3D& figs, const Matrix& m) {
+    for (auto& f : figs) applyTransformation(f, m);
+}
+
+img::EasyImage draw2DLines(const Lines2D& lines, const int size, Color background) {
+    // TODO: handle edge case
+    Line2D first = lines.front();
+
+    double xMin = first.p1.x;
+    double xMax = first.p1.x;
+
+    double yMin = first.p1.y;
+    double yMax = first.p1.y;
+
+    // Determine min and max
+    for (const Line2D& line : lines) {
+        if (line.p1.x < xMin) xMin = line.p1.x;
+        if (line.p2.x < xMin) xMin = line.p2.x;
+        if (line.p1.x > xMax) xMax = line.p1.x;
+        if (line.p2.x > xMax) xMax = line.p2.x;
+
+        if (line.p1.y < yMin) yMin = line.p1.y;
+        if (line.p2.y < yMin) yMin = line.p2.y;
+        if (line.p1.y > yMax) yMax = line.p1.y;
+        if (line.p2.y > yMax) yMax = line.p2.y;
+    }
+
+    // Compute variables needed for next step
+    double xRange = std::abs(xMax - xMin);
+    double yRange = std::abs(yMax - yMin);
+
+    double imageX = size * xRange / std::max(xRange, yRange);
+    double imageY = size * yRange / std::max(xRange, yRange);
+
+    double d = 0.95 * imageX / xRange;
+    double dcX = d * (xMin + xMax) / 2;
+    double dcY = d * (yMin + yMax) / 2;
+    double dX = imageX / 2 - dcX;
+    double dY = imageY / 2 - dcY;
+
+    // Create image
+    img::EasyImage img(std::lround(imageX), std::lround(imageY), background.toNative());
+
+    // Re-position points
+    for (Line2D line : lines) {
+        line.p1.x *= d;
+        line.p1.y *= d;
+
+        line.p1.x += dX;
+        line.p1.y += dY;
+
+        line.p1.x = std::lround(line.p1.x);
+        line.p1.y = std::lround(line.p1.y);
+
+        line.p2.x *= d;
+        line.p2.y *= d;
+
+        line.p2.x += dX;
+        line.p2.y += dY;
+
+        line.p2.x = std::lround(line.p2.x);
+        line.p2.y = std::lround(line.p2.y);
+
+        img.draw_line(line.p1.x, line.p1.y, line.p2.x, line.p2.y, line.color.toNative());
+    }
+
+    return img;
+}
+
+Point2D projectPoint(const Vector3D& point, const double d) {
+    double x = d * point.x / -point.z;
+    double y = d * point.y / -point.z;
+
+    return Point2D(x, y);
+}
+
+Lines2D projectFig(const Figure& fig) {
+    Lines2D lines;
+
+    for (auto face : fig.faces) {
+        for (unsigned int i = 0; i < face.pointIndexes.size(); i++) {
+            Vector3D p1 = fig.points[face.pointIndexes[i]];
+
+            Vector3D p2;
+            if (i + 1 >= face.pointIndexes.size()) p2 = fig.points[face.pointIndexes[0]];
+            else p2 = fig.points[face.pointIndexes[i + 1]];
+            Line2D line = Line2D(projectPoint(p1, 1.0), projectPoint(p2, 1.0), fig.color);
+
+            lines.push_back(line);
+        }
+
+
+    }
+
+    return lines;
+}
+
+Lines2D ProjectAll(const Figures3D& figs) {
+    Lines2D lines;
+
+    for (auto fig : figs) {
+        Lines2D figLines = projectFig(fig);
+        lines.insert(lines.end(), figLines.begin(), figLines.end());
+    }
+
+    return lines;
+}
+
+Lines2D projectFig(const Figure& fig) {
+    Lines2D lines;
+
+    for (auto face : fig.faces) {
+        for (unsigned int i = 0; i < face.pointIndexes.size(); i++) {
+            Vector3D p1 = fig.points[face.pointIndexes[i]];
+
+            Vector3D p2;
+            if (i + 1 >= face.pointIndexes.size()) p2 = fig.points[face.pointIndexes[0]];
+            else p2 = fig.points[face.pointIndexes[i + 1]];
+            Line2D line = Line2D(projectPoint(p1, 1.0), projectPoint(p2, 1.0), fig.color);
+
+            lines.push_back(line);
+        }
+
+
+    }
+
+    return lines;
+}
