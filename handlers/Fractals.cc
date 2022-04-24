9fa9016c2516502938d3878009b0a5ff9d5e86c8
Florrdv <flor@ronsmans.me>
2022-04-25 00:42:45 +0200

    Added scaling

--- handlers/Fractals.cc
+++ handlers/Fractals.cc
@@ -9,9 +9,10 @@ void generateFractal(Figure& fig, Figures3D& fractal, const int nr_iterations, c
     for (int i = 0; i < nr_iterations; i++) {
         for (Figure figure : fractal) {
             for (Vector3D point : figure.points) {
-                Vector3D newPoints;
-                for (Vector3D point : figure.points) {}
-                for (Face face : figure.faces) {}
+                std::vector<Vector3D> newPoints;
+                std::vector<Face> newFaces;
+                for (Vector3D point : figure.points) newPoints.push_back(point * scaleMatrix);
+                for (Face face : figure.faces) newFaces.push_back(face.clone());
                 Figure scaled = Figure();
             }
         }
