76879780be1337b4c5f4039028121d0087762446
Florrdv <flor@ronsmans.me>
2022-04-25 23:32:32 +0200

    Finished cube fractal setup

--- handlers/Fractals.cc
+++ handlers/Fractals.cc
@@ -5,21 +5,27 @@
 #include <cmath>
 
 void generateFractal(Figure& fig, Figures3D& fractal, const int nr_iterations, const double scale) {
-    Figure base = fig;
+    fractal.push_back(fig);
     Matrix scaleMatrix = transformations::scaleFigure(1 / scale);
 
     // Go through all iterations
     for (int i = 0; i < nr_iterations; i++) {
+        Figures3D intermediate;
         for (Figure figure : fractal) {
             for (Vector3D point : figure.points) {
+                Vector3D scaledPoint = point * scaleMatrix;
+                Vector3D translation = point - scaledPoint;
+
                 std::vector<Vector3D> newPoints;
                 std::vector<Face> newFaces;
-                for (Vector3D point : figure.points) newPoints.push_back(point * scaleMatrix);
+                for (Vector3D point : figure.points) newPoints.push_back(point * scaleMatrix + translation);
                 for (Face face : figure.faces) newFaces.push_back(face.clone());
                 Figure scaled = Figure(newPoints, newFaces, figure.color);
-                fractal.push_back(scaled);
+                intermediate.push_back(scaled);
             }
         }
+
+        fractal = intermediate;
     }
 }
 
